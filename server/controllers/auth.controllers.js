742c9ff072a9526efd115996ac15d357faac16ee
Mr-LeeB <quangbaokh9a2@gmail.com>
2023-05-20 18:08:45 +0700

    up

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -20,7 +20,7 @@ const login = async (req, res) => {
     }
   } catch (error) {
     console.log(error);
-    res.status(STATUS_CODE.SERVER_ERROR).send({ success: false, message: 'Internal server error' });
+    res.status(STATUS_CODE.SERVER_ERROR).send({ success: false, message: error });
   }
 };
 
